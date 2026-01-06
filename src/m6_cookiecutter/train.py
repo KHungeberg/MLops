3aa9cd94c967d9ceb113665920a438e4baa01963
KHungeberg <kristianulrikh@hotmail.com>
2026-01-06 15:04:26 +0100

    init cookiecutter project

---
+++ src/m6_cookiecutter/train.py
@@ -0,0 +1,10 @@
+from m6_cookiecutter.model import Model
+from m6_cookiecutter.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
